8d9b91285447d48b724d3bab9393ed052fa24c17
Ivan Sevilla <ivansevillaa@gmail.com>
2019-04-19 20:24:21 -0300

    Updates

---
+++ precache-manifest.ea7da1f6a148a0d8c6a2abc6c3cffb88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9026fa9f0bde7fbf1b6b1aeb9257792",
+    "url": "/podcasts/static/media/podcast-logo.d9026fa9.png"
+  },
+  {
+    "revision": "702e569e7c9bf77289e6",
+    "url": "/podcasts/static/js/runtime~main.757eefc9.js"
+  },
+  {
+    "revision": "77616ac2c2aa3cecc9db",
+    "url": "/podcasts/static/js/main.f31b4da1.chunk.js"
+  },
+  {
+    "revision": "fe99f1cc0c0e956c635e",
+    "url": "/podcasts/static/js/2.302e14ad.chunk.js"
+  },
+  {
+    "revision": "77616ac2c2aa3cecc9db",
+    "url": "/podcasts/static/css/main.35ef0f87.chunk.css"
+  },
+  {
+    "revision": "3af20641e1ad6c0b0983aa7e4f565ddb",
+    "url": "/podcasts/index.html"
+  }
+];
